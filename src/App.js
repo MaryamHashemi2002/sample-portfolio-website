ec083d36b7ccb1c6581fc84b42eb7b3fdfc147a7
maryam hashemi <mrymhshmi96@gmail.com>
2022-09-21 00:54:07 +0430

    tailwind successfully setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Contact, Contract, Portfolio, Home, Resume } from "./pages";
+import { Sidebar } from "./components";
+import "./App.css";
 
 function App() {
+  const activeMenu = true;
+
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <div className="flex relative">
+
+          {activeMenu ? (<div className="w-60 bg-black">Sidebar</div>) : (<div>Sidebar OFF</div>)}
+
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+
+
+export { default as Contact } from './Contact';
+export { default as Contract } from './Contract'
+export { default as Portfolio } from './Portfolio'
+export { default as Home } from './Home'
+export { default as Resume } from './Resume'
+
